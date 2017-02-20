ef4c9ddd60c68a42221dbe301f1e74be9d1109ea
Travis CI <manuel.leduc@inria.fr>
2017-02-20 18:31:56 +0100

    use the force

--- build.sh
+++ build.sh
@@ -46,7 +46,7 @@ function deploy {
 	git add -A
 	git status
 	git commit -m "Lastest site built on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to github"
-	git push $DEPLOY_REPO master:gh-pages
+	git push -f $DEPLOY_REPO master:gh-pages
 }
 
 
